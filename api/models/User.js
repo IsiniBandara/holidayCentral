e6cd44ad4e768c6cceecd0914d9bcde74d8685c9
IsiniBandara <67850663+IsiniBandara@users.noreply.github.com>
2023-06-05 09:15:51 +0530

    frontend and backend development

--- api/models/User.js
+++ api/models/User.js
@@ -1,17 +1,43 @@
+// import mongoose from "mongoose";
+
+// const UserSchema = new mongoose.Schema(
+//   {
+//     slct_permission: {
+//       type: String,
+//       required: true,
+//     },
+//     fname: {
+//       type: String,
+//       required: true,
+//       unique: true,
+//     },
+//     lname: {
+//       type: String,
+//       required: true,
+//       unique: true,
+//     },
+//     email: {
+//       type: String,
+//       required: true,
+//       unique: true,
+//     },
+//     password: {
+//       type: String,
+//       required: true,
+//     },
+//   },
+//   {
+//     collection: "UserInfo",
+//   }
+// );
+
+// export default mongoose.model("User", UserSchema);
+
 import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema(
   {
-    slct_permission: {
-      type: String,
-      required: true,
-    },
-    fname: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    lname: {
+    username: {
       type: String,
       required: true,
       unique: true,
@@ -21,13 +47,33 @@ const UserSchema = new mongoose.Schema(
       required: true,
       unique: true,
     },
+    country: {
+      type: String,
+      required: true,
+    },
+    img: {
+      type: String,
+    },
+    city: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+    },
     password: {
       type: String,
       required: true,
     },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
   },
+  { timestamps: true },
   {
-    collection: "UserInfo",
+    collection: "users",
   }
 );
 
